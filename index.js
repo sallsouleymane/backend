33c80f2346e295f21d1a07c23fbe3815c38d7c92
Azhar <azhar.saec@gmail.com>
2020-02-26 11:11:27 +0530

    update

--- index.js
+++ index.js
@@ -118,15 +118,15 @@ function makeotp(length) {
 }
 
 function sendSMS(content, mobile) {
-  // let url = "http://136.243.19.2/http-api.php?username=ewallet&password=bw@2019&senderid=EWALET&route=1&number=" + mobile + "&message=" + content;
-  // request(url, {
-  //   json: true
-  // }, (err, res, body) => {
-  //   if (err) {
-  //     return err;
-  //   }
-  //   return body;
-  // });
+  let url = "http://136.243.19.2/http-api.php?username=ewallet&password=bw@2019&senderid=EWALET&route=1&number=" + mobile + "&message=" + content;
+  request(url, {
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      return err;
+    }
+    return body;
+  });
    return '';
 }
 
