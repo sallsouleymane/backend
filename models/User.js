2f9e21f35bd164ae87434611bf6aec310879e81d
Azhar <azhar.saec@gmail.com>
2020-03-11 11:08:20 +0530

    user login

--- models/User.js
+++ models/User.js
@@ -2,20 +2,13 @@
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true},
-  username: { type: String, required: true, unique: true },
-  phone: { type: Number, required: true, unique: true },
+  mobile: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  token: { type: String, required: false }
+  address: {type: String, required: false},
+  token: { type: String, required: false },
+  otp: { type: String, required: false,  default: null},
+  status: {type:Number, required: true, default: 0}
 });
 module.exports = mongoose.model('User', UserSchema);
-
-UserSchema.methods.isCorrectPassword = function(password, callback){
-    // bcrypt.compare(password, this.password, function(err, same) {
-      if (password == this.password) {
-        callback(err);
-      } else {
-        callback(err, same);
-      }
-    // });
-  }
   
