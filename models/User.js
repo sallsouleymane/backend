a09246beb98259f4cc17d615f3f57dbda100378b
zainabmurtaza <bzainab61@gmail.com>
2020-04-06 11:51:35 +0530

    minor changes in fields name

--- models/User.js
+++ models/User.js
@@ -13,15 +13,15 @@ const UserSchema = new mongoose.Schema({
   id_type: { type: String, required: false },
   id_name: { type: String, required: false },
   valid_till: { type: String, required: false },
-  idNumber: { type: String, required: false },
+  id_number: { type: String, required: false },
   dob: { type: String, required: false },
   gender: { type: String, required: false },
   token: { type: String, required: false },
   otp: { type: String, required: false,  default: null },
   bank: { type: String, required: false, default: null },
-  docsHash: { type: Array, required: false, default: null },
+  docs_hash: { type: Array, required: false, default: null },
   status: { type: String, required: true, default: null },
-  contactList: { type: [String], required: false, default: null }
+  contact_list: { type: [String], required: false, default: null }
 });
 module.exports = mongoose.model('User', UserSchema);
   
