0fa2c5d9d6ca4cee8dcc3f4fae2747db33644394
zainabmurtaza <bzainab61@gmail.com>
2020-05-13 10:22:31 +0530

    change password api added

--- routes/Merchant.js
+++ routes/Merchant.js
@@ -13,3 +13,28 @@ const makeid = require("./utils/idGenerator");
 const makeotp = require("./utils/makeotp");
 const blockchain = require("../services/Blockchain");
 
+router.post("/merchant/changePassword", jwtTokenAuth, (req, res) => {
+    const { password } = req.body;
+    const username = req.username;
+	merchant.findOneAndUpdate(
+		{
+			username,
+		},
+		{
+			password: password,
+		},
+		function (err, merchant) {
+			if (err) {
+				res.status(200).json({
+					status: 0,
+					error: "Internal Server Error",
+				});
+			} else {
+				res.status(200).json({
+					status: 1,
+					message: "Updated password successfully",
+				});
+			}
+		}
+	);
+});
