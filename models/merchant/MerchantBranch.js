1ee9d79c12c7029eb63f55c7f2220b137073ebe8
zainabmurtaza <bzainab61@gmail.com>
2020-06-18 15:40:45 +0530

    last_paid_at is not required field

--- models/merchant/MerchantBranch.js
+++ models/merchant/MerchantBranch.js
@@ -25,7 +25,7 @@ const MerchantBranchSchema = new mongoose.Schema({
 	bills_raised: { type: Number, required: true, default: 0 },
 	amount_collected: { type: Number, required: true, default: 0 }, 
 	amount_collected_desc: { type: String, required: false, default: "Today's total amount paid"},
-	last_paid_at: { type: Date, required: true, default: null},
+	last_paid_at: { type: Date, required: false, default: null},
 	amount_due: { type: Number, required: true, default: 0 }
 });
 module.exports = mongoose.model("MerchantBranch", MerchantBranchSchema);
