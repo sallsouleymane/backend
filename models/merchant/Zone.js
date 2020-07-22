f8b5e7e0ac032000a82106deb82e1184aec26a0c
Adarsh Sharma <s.adarsh1996@gmail.com>
2020-07-22 20:23:16 +0530

    removed unique key from zone

--- models/merchant/Zone.js
+++ models/merchant/Zone.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const ZoneSchema = new mongoose.Schema({
     code: { type: String, required: true, unique: true },
-    merchant_id: { type: String, required: true, unique: true },
+    merchant_id: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: false },
     type: { type: String, required: false }
