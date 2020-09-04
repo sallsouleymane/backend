5ce627c5b78e11b04de5a428f4ee0ad7170d5939
mustafa-kmindz <bzainab61@gmail.com>
2020-09-04 10:03:43 +0530

    4 more apis of partner cashier added

--- routes/InvoicePay.js
+++ routes/InvoicePay.js
@@ -11,6 +11,7 @@ const { calculateShare } = require("./utils/utility");
 
 const cashierInvoicePay = require("./transactions/cashierInvoicePay");
 const userInvoicePay = require("./transactions/userInvoicePay");
+const partnerCashierInvoicePay = require("./transactions/partnerCashierInvoicePay");
 
 const Bank = require("../models/Bank");
 const Branch = require("../models/Branch");
@@ -24,9 +25,550 @@ const User = require("../models/User");
 const Invoice = require("../models/merchant/Invoice");
 const Commission = require("../models/merchant/BankCommission");
 const InvoiceGroup = require("../models/merchant/InvoiceGroup");
+const Partner = require("../models/partner/Partner");
+const PartnerCashier = require("../models/partner/Cashier");
+const PartnerBranch = require("../models/partner/Branch");
 
 const jwtTokenAuth = require("./JWTTokenAuth");
 
+router.post("/partnerCashier/payInvoice", jwtTokenAuth, (req, res) => {
+	const { invoice_ids, merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	PartnerCashier.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, cashier) {
+			if (err) {
+				console.log(err);
+				var message = err;
+				if (err.message) {
+					message = err.message;
+				}
+				res.status(200).json({
+					status: 0,
+					message: message,
+				});
+			} else if (cashier == null) {
+				res.status(200).json({
+					status: 0,
+					message:
+						"Token changed or user not valid. Try to login again or contact system administrator.",
+				});
+			} else {
+				Partner.findOne({ _id: cashier.partner_id }, (err, partner) => {
+					MerchantFee.findOne(
+						{ merchant_id: merchant_id, type: 1 },
+						(err, fee) => {
+							if (err) {
+								console.log(err);
+								var message = err;
+								if (err.message) {
+									message = err.message;
+								}
+								res.status(200).json({
+									status: 0,
+									message: message,
+								});
+							} else if (fee == null) {
+								res.status(200).json({
+									status: 0,
+									message: "Fee rule not found",
+								});
+							} else {
+								Commission.findOne(
+									{ merchant_id: merchant_id, type: 1 },
+									async (err, comm) => {
+										if (err) {
+											console.log(err);
+											var message = err;
+											if (err.message) {
+												message = err.message;
+											}
+											res.status(200).json({
+												status: 0,
+												message: message,
+											});
+										} else if (comm == null) {
+											res.status(200).json({
+												status: 0,
+												message: "Commission rule not found",
+											});
+										} else {
+											try {
+												var invoice;
+												var total_amount = 0;
+												for (invoice_id of invoice_ids) {
+													invoice = await Invoice.findOne({
+														_id: invoice_id,
+														merchant_id: merchant_id,
+														paid: 0,
+														is_validated: 1,
+													});
+													if (invoice == null) {
+														throw new Error(
+															"Invoice id " +
+															invoice_id +
+															" is already paid or it belongs to different merchant"
+														);
+													}
+													total_amount += invoice.amount;
+												}
+												if (total_amount < 0) {
+													throw new Error("Amount is a negative value");
+												}
+
+												// all the users
+												let branch = await PartnerBranch.findOne({
+													_id: cashier.branch_id,
+													status: 1,
+												});
+												if (branch == null) {
+													throw new Error("Cashier has invalid branch");
+												}
+
+												let bank = await Bank.findOne({
+													_id: partner.bank_id,
+													status: 1,
+												});
+												if (bank == null) {
+													throw new Error("Cashier's Partner has invalid bank");
+												}
+
+												// check branch operational wallet balance
+												const branchOpWallet =
+													branch.code + "_partnerbranch_operational@" + bank.name;
+												var bal = await blockchain.getBalance(branchOpWallet);
+												console.log(branchOpWallet);
+												if (Number(bal) < total_amount) {
+													res.status(200).json({
+														status: 0,
+														message: "Not enough balance. Recharge Your wallet.",
+													});
+												} else {
+													let infra = await Infra.findOne({
+														_id: bank.user_id,
+													});
+													if (infra == null) {
+														throw new Error("Cashier's bank has invalid infra");
+													}
+
+													let merchant = await Merchant.findOne({
+														_id: merchant_id,
+													});
+													if (merchant == null) {
+														throw new Error("Invoice has invalid merchant");
+													}
+
+													const today = new Date();
+													await Merchant.findOneAndUpdate(
+														{
+															_id: merchant._id,
+															last_paid_at: {
+																$lte: new Date(today.setHours(00, 00, 00)),
+															},
+														},
+														{ amount_collected: 0 }
+													);
+
+													var result = await partnerCashierInvoicePay(
+														total_amount,
+														infra,
+														bank,
+														branch,
+														merchant,
+														fee,
+														comm
+													);
+													var status_update_feedback;
+													if (result.status == 1) {
+														for (invoice_id of invoice_ids) {
+															var i = await Invoice.findOneAndUpdate(
+																{ _id: invoice_id },
+																{ paid: 1 }
+															);
+															if (i == null) {
+																status_update_feedback =
+																	"Invoice paid status can not be updated";
+															}
+
+															var last_paid_at = new Date();
+															var m = await Merchant.updateOne(
+																{ _id: merchant._id },
+																{
+																	last_paid_at: last_paid_at,
+																	$inc: {
+																		amount_collected: total_amount,
+																		amount_due: -total_amount,
+																		bills_paid: 1,
+																	},
+																}
+															);
+															if (m == null) {
+																status_update_feedback =
+																	"Merchant status can not be updated";
+															}
+
+															var mc = await MerchantCashier.updateOne(
+																{ _id: i.cashier_id },
+																{
+																	last_paid_at: last_paid_at,
+																	$inc: {
+																		bills_paid: 1,
+																	},
+																}
+															);
+															if (mc == null) {
+																status_update_feedback =
+																	"Merchant cashier status can not be updated";
+															}
+
+															var mb = await MerchantBranch.updateOne(
+																{ _id: mc.branch_id },
+																{
+																	last_paid_at: last_paid_at,
+																	$inc: {
+																		amount_collected: total_amount,
+																		amount_due: -total_amount,
+																		bills_paid: 1,
+																	},
+																}
+															);
+															if (mb == null) {
+																status_update_feedback =
+																	"Merchant branch status can not be updated";
+															}
+
+															var ig = await InvoiceGroup.updateOne(
+																{ _id: i.group_id },
+																{
+																	last_paid_at: last_paid_at,
+																	$inc: {
+																		bills_paid: 1,
+																	},
+																}
+															);
+															if (ig == null) {
+																status_update_feedback =
+																	"Invoice group status can not be updated";
+															}
+
+															bankFee = calculateShare("bank", total_amount, fee);
+															partnerFeeShare = calculateShare(
+																"partner",
+																total_amount,
+																fee
+															);
+															partnerCommShare = calculateShare(
+																"partner",
+																total_amount,
+																comm
+															);
+															var c = await PartnerCashier.updateOne(
+																{ _id: cashier._id },
+																{
+																	$inc: {
+																		cash_received: total_amount + bankFee,
+																		cash_in_hand: total_amount + bankFee,
+																		fee_generated: partnerFeeShare,
+																		commission_generated: partnerCommShare,
+																		total_trans: 1,
+																	},
+																}
+															);
+															if (c == null) {
+																status_update_feedback =
+																	"Bank cashier's status can not be updated";
+															}
+
+															content =
+																"E-Wallet:  Amount " +
+																i.amount +
+																" is paid for invoice nummber " +
+																i.number +
+																" for purpose " +
+																i.description;
+															sendSMS(content, invoice.mobile);
+														}
+													}
+													result.status_update_feedback = status_update_feedback;
+													res.status(200).json(result);
+												}
+											} catch (err) {
+												console.log(err);
+												var message = err;
+												if (err && err.message) {
+													message = err.message;
+												}
+												res
+													.status(200)
+													.json({ status: 0, message: message, err: err });
+											}
+										}
+									}
+								);
+							}
+						}
+					);
+				});
+			}
+		}
+	);
+});
+
+router.post("/partnerCashier/getInvoicesForCustomerCode", jwtTokenAuth, (req, res) => {
+	const { customer_code, merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	PartnerCashier.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, cashier) {
+			if (err) {
+				console.log(err);
+				var message = err;
+				if (err.message) {
+					message = err.message;
+				}
+				res.status(200).json({
+					status: 0,
+					message: message,
+				});
+			} else if (cashier == null) {
+				res.status(200).json({
+					status: 0,
+					message:
+						"Token changed or user not valid. Try to login again or contact system administrator.",
+				});
+			} else {
+				Partner.findOne({ _id: cashier.partner_id }, (err, partner) => {
+					Invoice.find(
+						{
+							is_validated: 1,
+							merchant_id: merchant_id,
+							customer_code: customer_code,
+						},
+						async (err, invoices) => {
+							if (err) {
+								console.log(err);
+								var message = err;
+								if (err.message) {
+									message = err.message;
+								}
+								res.status(200).json({
+									status: 0,
+									message: message,
+								});
+							} else if (invoices.length == 0) {
+								res.status(200).json({
+									status: 0,
+									message: "Invoice not found",
+								});
+							} else {
+								Merchant.findOne(
+									{
+										_id: merchant_id,
+										bank_id: partner.bank_id,
+										status: 1,
+									},
+									(err, merchant) => {
+										if (err) {
+											console.log(err);
+											var message = err;
+											if (err.message) {
+												message = err.message;
+											}
+											res.status(200).json({
+												status: 0,
+												message: message,
+											});
+										} else if (merchant == null) {
+											res.status(200).json({
+												status: 0,
+												message: "Invalid Merchant",
+											});
+										} else {
+											res.status(200).json({
+												status: 1,
+												invoice: invoices,
+											});
+										}
+									}
+								);
+							}
+						}
+					);
+				});
+			}
+		}
+	);
+});
+
+router.post("/partnerCashier/getInvoiceDetails", jwtTokenAuth, (req, res) => {
+	const { number, merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	PartnerCashier.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, cashier) {
+			if (err) {
+				console.log(err);
+				var message = err;
+				if (err.message) {
+					message = err.message;
+				}
+				res.status(200).json({
+					status: 0,
+					message: message,
+				});
+			} else if (cashier == null) {
+				res.status(200).json({
+					status: 0,
+					message:
+						"Token changed or user not valid. Try to login again or contact system administrator.",
+				});
+			} else {
+				Partner.findOne({ _id: cashier.partner_id }, (err, partner) => {
+					Invoice.find(
+						{
+							is_validated: 1,
+							merchant_id: merchant_id,
+							$or: [{ number: number }, { reference_invoice: number }],
+						},
+						async (err, invoices) => {
+							if (err) {
+								console.log(err);
+								var message = err;
+								if (err.message) {
+									message = err.message;
+								}
+								res.status(200).json({
+									status: 0,
+									message: message,
+								});
+							} else if (invoices.length == 0) {
+								res.status(200).json({
+									status: 0,
+									message: "Invoice not found",
+								});
+							} else {
+								Merchant.findOne(
+									{
+										_id: merchant_id,
+										bank_id: partner.bank_id,
+										status: 1,
+									},
+									(err, merchant) => {
+										if (err) {
+											console.log(err);
+											var message = err;
+											if (err.message) {
+												message = err.message;
+											}
+											res.status(200).json({
+												status: 0,
+												message: message,
+											});
+										} else if (merchant == null) {
+											res.status(200).json({
+												status: 0,
+												message: "Invalid Merchant",
+											});
+										} else {
+											res.status(200).json({
+												status: 1,
+												invoice: invoices,
+											});
+										}
+									}
+								);
+							}
+						}
+					);
+				});
+			}
+		}
+	);
+});
+
+router.post("/partnerCashier/getUserInvoices", jwtTokenAuth, (req, res) => {
+	const { mobile } = req.body;
+	const jwtusername = req.sign_creds.username;
+	PartnerCashier.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, cashier) {
+			if (err) {
+				console.log(err);
+				var message = err;
+				if (err.message) {
+					message = err.message;
+				}
+				res.status(200).json({
+					status: 0,
+					message: message,
+				});
+			} else if (cashier == null) {
+				res.status(200).json({
+					status: 0,
+					message:
+						"Token changed or user not valid. Try to login again or contact system administrator.",
+				});
+			} else {
+				Partner.findOne({ _id: cashier.partner_id }, (err, partner) => {
+					try {
+						Invoice.find(
+							{ mobile: mobile, is_validated: 1 },
+							async (err, invoices) => {
+								if (err) {
+									console.log(err);
+									var message = err;
+									if (err.message) {
+										message = err.message;
+									}
+									res.status(200).json({
+										status: 0,
+										message: message,
+									});
+								} else {
+									var invoicePromises = invoices.map(async (invoice) => {
+										var merchant = await Merchant.findOne({
+											_id: invoice.merchant_id,
+											bank_id: partner.bank_id,
+											status: 1,
+										});
+										if (merchant != null) {
+											return invoice;
+										}
+									});
+									var result = await Promise.all(invoicePromises);
+									res.status(200).json({
+										status: 1,
+										invoices: result,
+									});
+								}
+							}
+						);
+					} catch (err) {
+						console.log(err);
+						var message = err.toString();
+						if (err.message) {
+							message = err.message;
+						}
+						res.status(200).json({ status: 0, message: message });
+					}
+				})
+			}
+		}
+	);
+
+});
 router.post("/cashier/getInvoicesForCustomerCode", (req, res) => {
 	const { token, customer_code, merchant_id } = req.body;
 	Cashier.findOne(
---
+++ routes/transactions/partnerCashierInvoicePay.js
@@ -0,0 +1,203 @@
+//services
+const blockchain = require("../../services/Blockchain.js");
+const { getTransactionCode, calculateShare } = require("../utils/utility");
+
+module.exports = async function (
+	amount,
+	infra,
+	bank,
+	branch,
+	merchant,
+	fee,
+	comm
+) {
+	// receiver's wallet names
+	const branchOpWallet = branch.code + "_partnerbranch_operational@" + bank.name;
+	var creator = (merchant.creator == 1) ? "inframerchant" : "merchant";
+	const merchantOpWallet = merchant.username + "_" + creator + "_operational@" + bank.name;
+	// const bankOpWallet = "operational@" + bank.name;
+	// const infraOpWallet = "infra_operational@" + bank.name;
+
+	let master_code = getTransactionCode(branch.mobile, merchant.mobile);
+
+	// first transaction
+	amount = Number(amount);
+
+	let trans1 = {
+		from: branchOpWallet,
+		to: merchantOpWallet,
+		amount: amount,
+		note: "Bill amount",
+		email1: branch.email,
+		email2: merchant.email,
+		mobile1: branch.mobile,
+		mobile2: merchant.mobile,
+		fromName: branch.name,
+		toName: merchant.name,
+		master_code: master_code,
+		child_code: master_code + "1",
+	};
+
+	var result = await blockchain.initiateTransfer(trans1);
+
+	// return response
+	if (result.status == 0) {
+		result = {
+			status: 0,
+			message: "Transaction failed!",
+			blockchain_message: result.message,
+		};
+	} else {
+		result = {
+			status: 1,
+			message: "Transaction success!",
+			blockchain_message: result.message,
+		};
+	}
+	distributeRevenue(
+		amount,
+		infra,
+		bank,
+		branch,
+		merchant,
+		fee,
+		comm,
+		master_code
+	);
+	return result;
+};
+
+async function distributeRevenue(
+	amount,
+	infra,
+	bank,
+	branch,
+	merchant,
+	fee,
+	comm,
+	master_code
+) {
+	const branchOpWallet = branch.code + "_partnerbranch_operational@" + bank.name;
+	var creator = (merchant.creator == 1) ? "inframerchant" : "merchant";
+	const merchantOpWallet = merchant.username + "_" + creator + "_operational@" + bank.name;
+	const bankOpWallet = "operational@" + bank.name;
+	const infraOpWallet = "infra_operational@" + bank.name;
+
+	//second transaction
+	bankFee = calculateShare("bank", amount, fee);
+	console.log("Bank Fee: ", bankFee);
+
+	let trans2 = {
+		from: branchOpWallet,
+		to: bankOpWallet,
+		amount: bankFee,
+		note: "Bank fee on paid bill",
+		email1: branch.email,
+		email2: bank.email,
+		mobile1: branch.mobile,
+		mobile2: bank.mobile,
+		fromName: branch.name,
+		toName: bank.name,
+		master_code: master_code,
+		child_code: getTransactionCode(branch.mobile, bank.mobile) + "2",
+	};
+
+	await blockchain.initiateTransfer(trans2);
+
+	//third transaction
+	infraShare = calculateShare("infra", amount, fee);
+	console.log("Infra Share: ", infraShare);
+	let trans3 = {
+		from: bankOpWallet,
+		to: infraOpWallet,
+		amount: infraShare,
+		note: "Fee share on paid bill",
+		email1: bank.email,
+		email2: infra.email,
+		mobile1: bank.mobile,
+		mobile2: infra.mobile,
+		fromName: bank.name,
+		toName: infra.name,
+		master_code: master_code,
+		child_code: getTransactionCode(bank.mobile, infra.mobile) + "3",
+	};
+
+	await blockchain.initiateTransfer(trans3);
+
+	//fourth transaction
+	partnerShare = calculateShare("partner", amount, fee);
+	console.log("Partner Share: ", partnerShare);
+	let trans4 = {
+		from: bankOpWallet,
+		to: branchOpWallet,
+		amount: partnerShare,
+		note: "Fee share on paid bill",
+		email1: bank.email,
+		email2: branch.email,
+		mobile1: bank.mobile,
+		mobile2: branch.mobile,
+		fromName: bank.name,
+		toName: branch.name,
+		master_code: master_code,
+		child_code: getTransactionCode(bank.mobile, branch.mobile) + "4",
+	};
+
+	await blockchain.initiateTransfer(trans4);
+
+	//fifth transaction
+	bankComm = calculateShare("bank", amount, comm);
+	let trans5 = {
+		from: merchantOpWallet,
+		to: bankOpWallet,
+		amount: bankComm,
+		note: "Bank commission on paid bill",
+		email1: merchant.email,
+		email2: bank.email,
+		mobile1: merchant.mobile,
+		mobile2: bank.mobile,
+		fromName: merchant.name,
+		toName: bank.name,
+		master_code: master_code,
+		child_code: getTransactionCode(merchant.mobile, bank.mobile) + "5",
+	};
+
+	await blockchain.initiateTransfer(trans5);
+
+	//sixth transaction
+	infraShare = calculateShare("infra", amount, comm);
+	let trans6 = {
+		from: bankOpWallet,
+		to: infraOpWallet,
+		amount: infraShare,
+		note: "Commission share on paid bill",
+		email1: bank.email,
+		email2: infra.email,
+		mobile1: bank.mobile,
+		mobile2: infra.mobile,
+		fromName: bank.name,
+		toName: infra.name,
+		master_code: master_code,
+		child_code: getTransactionCode(bank.mobile, infra.mobile) + "6",
+	};
+
+	await blockchain.initiateTransfer(trans6);
+
+	//seventh transaction
+	partnerShare = calculateShare("partner", amount, comm);
+	let trans7 = {
+		from: bankOpWallet,
+		to: branchOpWallet,
+		amount: partnerShare,
+		note: "Commission share on paid bill",
+		email1: bank.email,
+		email2: branch.email,
+		mobile1: bank.mobile,
+		mobile2: branch.mobile,
+		fromName: branch.name,
+		toName: bank.name,
+		master_code: master_code,
+		child_code: getTransactionCode(bank.mobile, branch.mobile) + "7",
+	};
+
+	await blockchain.initiateTransfer(trans7);
+}
