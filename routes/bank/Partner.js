8cd116aacdc0e9199971c9ddeccbde9bfe6a3e69
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-01-30 13:18:46 +0530

    modify block partnr API to block branches and cashiers too

--- routes/bank/Partner.js
+++ routes/bank/Partner.js
@@ -21,6 +21,8 @@ const { errorMessage, catchError } = require("../utils/errorHandler");
 const Bank = require("../../models/Bank");
 const OTP = require("../../models/OTP");
 const Partner = require("../../models/partner/Partner");
+const PartnerBranch = require("../../models/partner/Branch");
+const PartnerCashier = require("../../models/partner/Cashier");
 const Document = require("../../models/Document");
 
 router.post("/bank/blockPartner", jwtTokenAuth, function (req, res) {
@@ -52,10 +54,43 @@ router.post("/bank/blockPartner", jwtTokenAuth, function (req, res) {
 						if (result.status == 0) {
 							res.status(200).json(result);
 						} else {
-							res.status(200).json({
-								status: 1,
-								message: "blocked Partner",
-							});
+							PartnerBranch.updateMany(
+								{partner_id: partner_id},
+								{
+									$set: {
+										status: -1,
+									},
+								},
+								(err, branches) => {
+									let result = errorMessage(err, partner, "Branchs not found");
+									if (result.status == 0) {
+										res.status(200).json(result);
+									} else {
+										PartnerCashier.updateMany(
+											{partner_id: partner_id},
+											{
+												$set: {
+													status: -1,
+												},
+											},
+											(err, cashiers) => {
+												let result = errorMessage(err, partner, "Cashiers not found");
+												if (result.status == 0) {
+													res.status(200).json(result);
+												} else {
+													res.status(200).json({
+														status: 1,
+														message: "blocked Partner",
+													});
+												}
+											}
+
+										);
+
+									}
+								}
+							);
+							
 						}
 					}
 				);
