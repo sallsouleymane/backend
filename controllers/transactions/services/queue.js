33ed902068f7cff03747466e913821e27f7ec3c9
hlf-dev-zm <bzainab61@gmail.com>
2021-02-12 11:52:46 +0530

    Removed branch credit limit when checking for balance. Affected: Non Wallet to Wallet

--- controllers/transactions/services/queue.js
+++ controllers/transactions/services/queue.js
@@ -2,25 +2,29 @@ var amqp = require("amqplib/callback_api");
 var config = require("../../../config.json");
 
 module.exports.send = function (queue_name, transaction) {
-	amqp.connect("amqp://" + config.rabbitmqIP, function (error0, connection) {
-		if (error0) {
-			console.log(error0);
-		}
-		connection.createChannel(function (error1, channel) {
-			if (error1) {
-				console.log(error1);
+	try {
+		amqp.connect("amqp://" + config.rabbitmqIP, function (error0, connection) {
+			if (error0) {
+				console.log(error0);
 			}
+			connection.createChannel(function (error1, channel) {
+				if (error1) {
+					console.log(error1);
+				}
 
-			var queue = queue_name;
-			var msg = Buffer.from(JSON.stringify(transaction));
+				var queue = queue_name;
+				var msg = Buffer.from(JSON.stringify(transaction));
 
-			channel.assertQueue(queue, {
-				durable: false,
+				channel.assertQueue(queue, {
+					durable: false,
+				});
+				channel.sendToQueue(queue, Buffer.from(msg));
+
+				console.log(" [x] Sent %s to queue %s", msg, queue_name);
+				connection.close();
 			});
-			channel.sendToQueue(queue, Buffer.from(msg));
-
-			console.log(" [x] Sent %s to queue %s", msg, queue_name);
-			connection.close();
 		});
-	});
+	} catch (err) {
+		console.log(err);
+	}
 };
