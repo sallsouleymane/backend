92cfa5d8ad50b7a82f12b91903828c80323e620b
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-02-15 14:14:40 +0530

    bug fix

--- routes/Branch.js
+++ routes/Branch.js
@@ -79,7 +79,6 @@ router.post("/getBranchDashStats", jwtTokenAuth, function (req, res) {
 
 										Cashier.aggregate(
 											[
-												{ $match : { branch_id: user._id} },
 												{
 													$group: {
 														_id: null,
