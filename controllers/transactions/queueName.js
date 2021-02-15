d81bd64bb41329dc360b19b1a0f4b6040be615ee
hlf-dev-zm <bzainab61@gmail.com>
2021-02-15 12:35:58 +0530

    Removed bank escrow wallet and bank operational wallet check. Affected: Non Wallet to Non Wallet

---
+++ controllers/transactions/queueName.js
@@ -0,0 +1,27 @@
+const FEE = "FEE";
+const COMM = "COMM";
+const INFRAFIXED = "INFRAFIXED";
+const INFRAPERCENT = "INFRAPERCENT";
+const SENDFEE = "SENDFEE";
+const CLAIMFEE = "CLAIMFEE";
+const BANKMASTER = "BANKMASTER";
+const INFRAMASTER = "INFRAMASTER";
+const CLAIMMASTER = "CLAIMMASTER";
+const SENDMASTER = "SENDMASTER";
+const PARTNERSHARE = "PARTNERSHARE";
+const GENERAL = "GENERAL";
+
+module.exports = {
+	fee: FEE,
+	comm: COMM,
+	infra_fixed: INFRAFIXED,
+	infra_percent: INFRAPERCENT,
+	send_fee: SENDFEE,
+	claim_fee: CLAIMFEE,
+	bank_master: BANKMASTER,
+	infra_master: INFRAMASTER,
+	claim_master: CLAIMMASTER,
+	send_master: SENDMASTER,
+	partner_share: PARTNERSHARE,
+	general: GENERAL,
+};
