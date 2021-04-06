f00829a5c554e7b224faad783bc0ee0237c9e1f9
hlf-dev-zm <bzainab61@gmail.com>
2021-04-06 10:27:35 +0530

    added txstate model path

--- controllers/transactions/interBank/cashierClaimMoney.js
+++ controllers/transactions/interBank/cashierClaimMoney.js
@@ -11,6 +11,10 @@ const qname = require("../constants/queueName");
 const categoryConst = require("../constants/category");
 const childType = require("../constants/childType");
 
+//models
+const TxState = require("../../../models/TxState.js");
+const Infra = require("../../../models/Infra");
+
 module.exports = async function (
 	transfer,
 	sendingBank,
