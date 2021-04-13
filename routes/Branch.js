6c783139f73072162d715ba0f124b5dbc1547759
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-04-13 09:41:09 +0530

    bug fix

--- routes/Branch.js
+++ routes/Branch.js
@@ -633,6 +633,67 @@ router.post("/updateCashierTransferStatus", jwtTokenAuth, function (req, res) {
 	);
 });
 
+router.post(
+	"/branch/updateCashierUser",
+	jwtTokenAuth,
+	function (req, res) {
+		const { cashier_id, user_id } = req.body;
+		const jwtusername = req.sign_creds.username;
+		Branch.findOne(
+			{
+				username: jwtusername,
+				status: 1,
+			},
+			function (err, branch) {
+				let result = errorMessage(
+					err,
+					branch,
+					"Token changed or user not valid. Try to login again or contact system administrator."
+				);
+				if (result.status == 0) {
+					res.status(200).json(result);
+				} else {
+					Cashier.countDocuments(
+						{ bank_user_id: user_id },
+						function (err, count) {
+							if (count > 0) {
+								res.status(200).json({
+									status: 0,
+									message:
+										"User is already assigned to this or another cashier",
+								});
+							} else {
+								Cashier.findByIdAndUpdate(
+									cashier_id,
+									{ bank_user_id: user_id },
+									function (err, cashier) {
+										if (err) {
+											console.log(err);
+											var message = err;
+											if (err.message) {
+												message = err.message;
+											}
+											res.status(200).json({
+												status: 0,
+												message: message,
+											});
+										} else {
+											res.status(200).json({
+												status: 1,
+												row: cashier,
+											});
+										}
+									}
+								);
+							}
+						}
+					);
+				}
+			}
+		);
+	}
+);
+
 router.post("/branch/getCashierDetails", jwtTokenAuth, function (req, res) {
 	const { cashier_id } = req.body;
 
