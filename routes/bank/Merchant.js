6e83ef480a3197e17af61ee24fe148ebe7d4dbb3
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-04-15 23:42:08 +0530

    bug fix

--- routes/bank/Merchant.js
+++ routes/bank/Merchant.js
@@ -15,10 +15,13 @@ const { createWallet } = require("../../services/Blockchain.js");
 const Bank = require("../../models/Bank");
 const Merchant = require("../../models/merchant/Merchant");
 const MerchantBranch = require("../../models/merchant/MerchantBranch");
+const Zone = require("../../models/merchant/Zone");
+const Subzone = require("../../models/merchant/Subzone");
 const MerchantPosition = require("../../models/merchant/Position");
 const getWalletIds = require("../utils/getWalletIds");
 const InvoiceGroup = require("../../models/merchant/InvoiceGroup");
 const Invoice = require("../../models/merchant/Invoice");
+const MerchantSettings = require("../../models/merchant/MerchantSettings");
 
 router.post("/bank/changeMerchantAcces", jwtTokenAuth, function (req, res) {
 	const { merchant_id, is_private } = req.body;
@@ -658,4 +661,605 @@ router.post("/bank/getMerchantDashStats", jwtTokenAuth, function (req, res) {
 		}
 	);
 });
+
+router.post("/bank/getMerchantZoneList", jwtTokenAuth, (req, res) => {
+	const { merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+		
+				Zone.find({ merchant_id: merchant_id }, async (err, zones) => {
+					if (err) {
+						console.log(err);
+						var message = err;
+						if (err.message) {
+							message = err.message;
+						}
+						res.status(200).json({
+							status: 0,
+							message: message,
+						});
+					} else {
+						res.status(200).json({
+							status: 1,
+							list: zones,
+						});
+					}
+				});
+			}
+			
+		}
+	);
+});
+
+router.post("/bank/getMerchantSubzoneList", jwtTokenAuth, (req, res) => {
+	const { merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+			
+				Subzone.find({ merchant_id: merchant_id }, async (err, subzones) => {
+					if (err) {
+						console.log(err);
+						var message = err;
+						if (err.message) {
+							message = err.message;
+						}
+						res.status(200).json({
+							status: 0,
+							message: message,
+						});
+					} else {
+						res.status(200).json({
+							status: 1,
+							list: subzones,
+						});
+					}
+				});
+			}
+			
+		}
+	);
+});
+
+router.post("/bank/getMerchantBranchList", jwtTokenAuth, (req, res) => {
+	const { merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+				MerchantBranch.find({ merchant_id: merchant_id }, async (err, branches) => {
+					if (err) {
+						console.log(err);
+						var message = err;
+						if (err.message) {
+							message = err.message;
+						}
+						res.status(200).json({
+							status: 0,
+							message: message,
+						});
+					} else {
+						res.status(200).json({
+							status: 1,
+							list: branches,
+						});
+					}
+				});
+			}
+		}
+	);
+});
+
+router.post("/bank/getMerchantSettings", jwtTokenAuth, function (req, res) {
+	const { merchant_id } = req.body;
+	const jwtusername = req.sign_creds.username;
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+				MerchantSettings.findOne(
+					{ merchant_id: merchant_id },
+					(err, setting) => {
+						if (err) {
+							console.log(err);
+							var message = err;
+							if (err.message) {
+								message = err.message;
+							}
+							res.status(200).json({
+								status: 0,
+								message: message,
+							});
+						} else if (!setting) {
+							res.status(200).json({
+								status: 0,
+								message: "Setting Not found",
+							});
+						} else {
+							res.status(200).json({
+								status: 1,
+								setting: setting,
+							});
+						}
+					}
+				);
+			}
+		}
+	);
+});
+
+router.post("/bank/:type/getMerchantStatsBydate",jwtTokenAuth,function (req, res) {
+	const jwtusername = req.sign_creds.username;
+	const type = req.params.type;
+	const { id, date } = req.body;
+	var today = new Date(date);
+	today = today.toISOString();
+	var s = today.split("T");
+	var start = s[0] + "T00:00:00.000Z";
+	var end = s[0] + "T23:59:59.999Z";
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+
+				Invoice.aggregate(
+					[
+						{
+							$match: {
+								[`${type}_id`] : id,
+								created_at: {
+									$gte: new Date(
+										start
+									),
+									$lte: new Date(
+										end
+									),
+								},
+								paid:1,
+							},
+						},
+						{
+							$group: {
+								_id: "$paid_by",
+								amount_paid: { $sum: "$amount" },
+								penalty: { $sum: "$penalty"},
+								bills_paid: { $sum: 1 },
+							},
+						},
+					],async (err, post6) => {
+						let result = errorMessage(
+							err,
+							post6,
+							"Error."
+						);
+						if (result.status == 0) {
+							res.status(200).json(result);
+						} else {
+							Invoice.aggregate(
+								[
+									{
+										$match: {
+											[`${type}_id`] : id,
+											created_at: {
+												$gte: new Date(
+													start
+												),
+												$lte: new Date(
+													end
+												),
+											},
+										},
+									},
+									{
+										$group: {
+											_id: null,
+											amount_generated: { $sum: "$amount" },
+											bills_generated: { $sum: 1 },
+										},
+									},
+								],async (err, post7) => {
+									let result = errorMessage(
+										err,
+										post7,
+										"Error."
+									);
+									if (result.status == 0) {
+										res.status(200).json(result);
+									} else {
+										let ag = 0;
+										let bg = 0;
+										let InvoicePaidByMC = 0;
+										let InvoicePaidByBC = 0;
+										let InvoicePaidByPC = 0;
+										let InvoicePaidByUS = 0;
+										let AmountPaidByMC = 0;
+										let AmountPaidByBC = 0;
+										let AmountPaidByPC = 0;
+										let AmountPaidByUS = 0;
+										let InvoicePaid = 0;
+										let AmountPaid = 0;
+										if (
+											post7 != undefined &&
+											post7 != null &&
+											post7.length > 0
+										) {
+											ag = post7[0].amount_generated;
+											bg = post7[0].bills_generated;
+										}
+										if (
+											post6 != undefined &&
+											post6 != null &&
+											post6.length > 0
+										) {
+											const PaidByMC = await post6.filter((val) => {
+												return val._id==='MC'
+											});
+											const PaidByBC = await post6.filter((val) => {
+												return val._id==='BC'
+											});
+											const PaidByPC = await post6.filter((val)=>{
+												return val._id==='PC'
+											});
+											const PaidByUS = await post6.filter((val)=>{
+												return val._id==='US'
+											});
+											if(PaidByMC.length > 0){
+												InvoicePaidByMC = PaidByMC[0].bills_paid;
+												AmountPaidByMC = PaidByMC[0].amount_paid + PaidByMC[0].penalty;
+											}
+											if(PaidByBC.length > 0){
+												InvoicePaidByBC = PaidByBC[0].bills_paid;
+												AmountPaidByBC = PaidByBC[0].amount_paid + PaidByBC[0].penalty;
+											}
+											if(PaidByPC.length > 0){
+												InvoicePaidByPC = PaidByPC[0].bills_paid;
+												AmountPaidByPC = PaidByPC[0].amount_paid + PaidByPC[0].penalty;
+											}
+											if(PaidByUS.length > 0){
+												InvoicePaidByUS = PaidByUS[0].bills_paid;
+												AmountPaidByUS = PaidByUS[0].amount_paid + PaidByUS[0].penalty;
+											}
+
+											InvoicePaid = await post6.reduce((a, b) => {
+												return a + b.bills_paid;
+											}, 0);
+											
+											AmountPaid = await post6.reduce((a, b) => {
+												return a + b.amount_paid;
+											}, 0);
+										}
+										res.status(200).json({
+											status: 1,
+											amount_generated: ag,
+											bill_generated: bg,
+											amount_paid: AmountPaid,
+											bill_paid: InvoicePaid,
+											bill_paid_by_MC : InvoicePaidByMC,
+											amount_paid_by_MC: AmountPaidByMC,
+											bill_paid_by_PC : InvoicePaidByPC,
+											amount_paid_by_PC: AmountPaidByPC,
+											bill_paid_by_BC : InvoicePaidByBC,
+											amount_paid_by_BC: AmountPaidByBC,
+											bill_paid_by_US : InvoicePaidByUS,
+											amount_paid_by_US: AmountPaidByUS,
+											post7:post7,
+											post6:post6,
+										});
+									}
+								}
+							);
+						}
+					}		
+				);
+			}
+		}
+	);
+});
+
+router.post("/bank/:type/getMerchantStatsByPeriod",jwtTokenAuth,function (req, res) {
+	const jwtusername = req.sign_creds.username;
+	const type = req.params.type;
+	const { id, period_name } = req.body;
+	Bank.findOne(
+		{
+			username: jwtusername,
+			status: 1,
+		},
+		function (err, bank) {
+			let result = errorMessage(
+				err,
+				bank,
+				"Token changed or user not valid. Try to login again or contact system administrator."
+			);
+			if (result.status == 0) {
+				res.status(200).json(result);
+			} else {
+				Invoice.aggregate(
+					[
+						{
+							$match: {
+								[`${type}_id`] : id,
+								"bill_period.period_name": period_name,
+								paid: 1,
+							},
+						},
+						{
+							$group: {
+								_id: "$paid_by", 
+								amount_paid: { $sum: "$amount" },
+								bills_paid: { $sum: 1 },
+								penalty: { $sum: "$penalty"},
+							},
+						},
+					],async (err, post6) => {
+						let result = errorMessage(
+							err,
+							post6,
+							"Error."
+						);
+						if (result.status == 0) {
+							res.status(200).json(result);
+						} else {
+							Invoice.aggregate(
+								[
+									{
+										$match: {
+											[`${type}_id`] : id,
+											"bill_period.period_name": period_name,
+										},
+									},
+									{
+										$group: {
+											_id: null,
+											amount_generated: { $sum: "$amount" },
+											bills_generated: { $sum: 1 },
+										},
+									},
+								],async (err, post7) => {
+									let result = errorMessage(
+										err,
+										post7,
+										"Error."
+									);
+									if (result.status == 0) {
+										res.status(200).json(result);
+									} else {
+										let ag = 0;
+										let bg = 0;
+										let InvoicePaidByMC = 0;
+										let InvoicePaidByBC = 0;
+										let InvoicePaidByPC = 0;
+										let InvoicePaidByUS = 0;
+										let AmountPaidByMC = 0;
+										let AmountPaidByBC = 0;
+										let AmountPaidByPC = 0;
+										let AmountPaidByUS = 0;
+										let InvoicePaid = 0;
+										let AmountPaid = 0;
+										if (
+											post7 != undefined &&
+											post7 != null &&
+											post7.length > 0
+										) {
+											ag = post7[0].amount_generated;
+											bg = post7[0].bills_generated;
+										}
+										if (
+											post6 != undefined &&
+											post6 != null &&
+											post6.length > 0
+										) {
+											const PaidByMC = await post6.filter((val) => {
+												return val._id==='MC'
+											});
+											const PaidByBC = await post6.filter((val) => {
+												return val._id==='BC'
+											});
+											const PaidByPC = await post6.filter((val)=>{
+												return val._id==='PC'
+											});
+											const PaidByUS = await post6.filter((val)=>{
+												return val._id==='US'
+											});
+											if(PaidByMC.length > 0){
+												InvoicePaidByMC = PaidByMC[0].bills_paid;
+												AmountPaidByMC = PaidByMC[0].amount_paid + PaidByMC[0].penalty;
+											}
+											if(PaidByBC.length > 0){
+												InvoicePaidByBC = PaidByBC[0].bills_paid;
+												AmountPaidByBC = PaidByBC[0].amount_paid + PaidByBC[0].penalty;
+											}
+											if(PaidByPC.length > 0){
+												InvoicePaidByPC = PaidByPC[0].bills_paid;
+												AmountPaidByPC = PaidByPC[0].amount_paid + PaidByPC[0].penalty;
+											}
+											if(PaidByUS.length > 0){
+												InvoicePaidByUS = PaidByUS[0].bills_paid;
+												AmountPaidByUS = PaidByUS[0].amount_paid + PaidByUS[0].penalty;
+											}
+
+											InvoicePaid = await post6.reduce((a, b) => {
+												return a + b.bills_paid;
+											}, 0);
+											
+											AmountPaid = await post6.reduce((a, b) => {
+												return a + b.amount_paid;
+											}, 0);
+										}
+										res.status(200).json({
+											status: 1,
+											amount_generated: ag,
+											bill_generated: bg,
+											amount_paid: AmountPaid,
+											bill_paid: InvoicePaid,
+											bill_paid_by_MC : InvoicePaidByMC,
+											amount_paid_by_MC: AmountPaidByMC,
+											bill_paid_by_PC : InvoicePaidByPC,
+											amount_paid_by_PC: AmountPaidByPC,
+											bill_paid_by_BC : InvoicePaidByBC,
+											amount_paid_by_BC: AmountPaidByBC,
+											bill_paid_by_US : InvoicePaidByUS,
+											amount_paid_by_US: AmountPaidByUS,
+											post7:post7,
+											post6:post6,
+										});
+									}
+								}
+							);
+						}
+					}		
+				);
+			}
+		}
+	);
+});
+
+router.post("/bank/listMerchantSubzonesByZoneId",jwtTokenAuth,function (req, res) {
+		const { zone_id } = req.body;
+		const jwtusername = req.sign_creds.username;
+		Bank.findOne(
+			{
+				username: jwtusername,
+				status: 1,
+			},
+			function (err, bank) {
+				let result = errorMessage(
+					err,
+					bank,
+					"Token changed or user not valid. Try to login again or contact system administrator."
+				);
+				if (result.status == 0) {
+					res.status(200).json(result);
+				} else {
+					Subzone.find(
+						{ zone_id: zone_id },
+						function (err, subzone) {
+							if (err) {
+								console.log(err);
+								var message = err;
+								if (err.message) {
+									message = err.message;
+								}
+								res.status(200).json({
+									status: 0,
+									message: message,
+								});
+							} else {
+								res.status(200).json({
+									status: 1,
+									subzones: subzone,
+								});
+							}
+						}
+					);
+				}
+			}
+		);
+	}
+);
+
+router.post(
+	"/bank/listMerchantBranchesBySubzoneId",
+	jwtTokenAuth,
+	function (req, res) {
+		const { subzone_id } = req.body;
+		const jwtusername = req.sign_creds.username;
+		Bank.findOne(
+			{
+				username: jwtusername,
+				status: 1,
+			},
+			function (err, bank) {
+				let result = errorMessage(
+					err,
+					bank,
+					"Token changed or user not valid. Try to login again or contact system administrator."
+				);
+				if (result.status == 0) {
+					res.status(200).json(result);
+				} else {
+					MerchantBranch.find(
+						{  subzone_id: subzone_id },
+						function (err, branch) {
+							if (err) {
+								console.log(err);
+								var message = err;
+								if (err.message) {
+									message = err.message;
+								}
+								res.status(200).json({
+									status: 0,
+									message: message,
+								});
+							} else {
+								res.status(200).json({
+									status: 1,
+									branches: branch,
+								});
+							}
+						}
+					);
+				}
+				
+			}
+		);
+	}
+);
+
 module.exports = router;
